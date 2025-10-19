4c17fe43c3c566762b8b74080b58074a10708f74
Mat M. <github@mat-m.com>
2025-10-19 15:43:56 -0300

    chore: remove unused appconfig values

--- proxy/src/proxy/appconfig.clj
+++ proxy/src/proxy/appconfig.clj
@@ -2,12 +2,5 @@
 
 (def log-level (or (System/getenv "LOG_LEVEL") "info"))
 
-(def proxy-port
+(def http-proxy-port
   (or (System/getenv "PROXY_PORT") "4445"))
-
-(def duckt-server-url
-  (or (System/getenv "DUCKT_SERVER_URL") "https://api.duckt.dev"))
-
-;; token format: base64 encoded string of the form "<version>:<proxy-uuid>:<secret>"
-(def proxy-token
-  (System/getenv "PROXY_TOKEN"))
